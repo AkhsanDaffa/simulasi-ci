44bea6121d8c7109d9f745cbab32b49be77282dc
ADAP2001 <akhsandaffa01@gmail.com>
2025-12-01 11:27:20 +0700

    Menambahkan fitur cek saldo

--- test_bank.py
+++ test_bank.py
@@ -12,6 +12,10 @@ class TestBankAccount(unittest.TestCase):
         akun = BankAccount(100)
         hasil = akun.tarik(30)
         self.assertEqual(hasil, 70)
+    
+    def test_cek_saldo(self):
+        akun = BankAccount(100)
+        self.assertEqual(akun.cek_saldo(), 100)
 
 if __name__ == '__main__':
     unittest.main()
