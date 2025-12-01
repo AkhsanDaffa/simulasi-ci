9233aa50c92e94a3d4d7a8833d5ad3758cd349aa
ADAP2001 <akhsandaffa01@gmail.com>
2025-12-01 10:37:27 +0700

    Fix unit test error

--- test_bank.py
+++ test_bank.py
@@ -6,7 +6,7 @@ class TestBankAccount(unittest.TestCase):
     def test_setor_uang(self):
         akun = BankAccount(100)
         hasil = akun.setor(50)
-        self.assertEqual(hasil, 9999)
+        self.assertEqual(hasil, 150)
     
     def test_tarik_uang(self):
         akun = BankAccount(100)
