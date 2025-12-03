1960dd38462bd774e5b4c1059ed637170945f100
ADAP2001 <akhsandaffa01@gmail.com>
2025-12-03 08:32:18 +0700

    Ubah jadi Web App Flask

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM python:3.9-slim
 
 WORKDIR /app
 
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
 COPY . .
 
-CMD ["python", "test_bank.py"]
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from bank import BankAccount
+import os
+
+app = Flask(__name__)
+
+akun = BankAccount(1000)
+
+@app.route('/')
+def home():
+    return """
+    <h1>Halo! Ini Aplikasi Bank Saya </h1>
+    <p>Aplikasi ini berjalan di dalam Docker Container!</p>
+    <p>Coba cek saldo di: <a href="/saldo">/saldo</a></p>
+"""
+
+@app.route('/saldo')
+def cek_saldo():
+    jumlah = akun.cek_saldo()
+    return f"<h2>Saldo Anda saat ini: Rp {jumlah}</h2>"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
